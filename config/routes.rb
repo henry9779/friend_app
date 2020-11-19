16da6f5f4ec93c116d7f88d91784f79ceea1cc50
Henry <vinsen1109@gmail.com>
2020-11-19 15:27:27 +0800

    change path

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
   resources :friends
   get 'home/about' #一般路徑
   root 'home#index' #設定首頁，越上面越優先
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
 end
